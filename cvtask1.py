761888d2bc2b6a8993f114f36441dcc033cb9720
S-taha102 <taha15370@gmail.com>
2025-01-20 01:59:59 +0200

    s

--- cvtask1.py
+++ cvtask1.py
@@ -5,8 +5,7 @@ face=cv.CascadeClassifier('face.xml')
 def detect(frame):
     gray=cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
     
-    face_detect=face.detectMultiScale(gray,scaleFactor=1.1,
-        minNeighbors=3)
+    face_detect=face.detectMultiScale(gray,1.1,3)
     
     for(x,y,w,h) in face_detect:
         cv.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
